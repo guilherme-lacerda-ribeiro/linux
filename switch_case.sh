10479526030dfa7763119d4fda2203c4ee20762b
guilherme-lacerda-ribeiro <guilhermelacerdaribeiro@gmail.com>
2025-02-24 13:37:43 -0300

    indent fix

--- switch_case.sh
+++ switch_case.sh
@@ -3,20 +3,25 @@
 read -p "Entre com a operação desejada: 'compactar' ou 'descompactar'" operacao
 
 case "$operacao" in
-        "compactar")
-                read -p "Nome do arquivo final (.tar.gz)" arquivo_saida
-                read -p "Lista de arquivos separados por espaço:" arquivos
-                tar -czf "$arquivo_saida" $arquivos
-                echo "Compactados com sucesso em $arquivo_saida"
-        ;;
-        "descompactar")
-                read -p "Nome do arquivo a descompactar (.tar.gz)" arquivo
-                read -p "Diretorio de destino" diretorio
-                tar -xzf "$arquivo" -C "$diretorio"
-                echo "Descompactado com sucesso em $diretorio"
-        ;;
-        *)
-        echo "Operacao invalida!"
-        echo "Selecione descompactar ou compactar"
-        exit 1
+  "compactar")
+    read -p "Nome do arquivo de saída (.tar.gz): " arquivo_saida
+    read -p "Lista de arquivos a compactar (separados por espaço): " arquivos
+    tar -czf "$arquivo_saida" $arquivos
+    echo "Arquivos compactados com sucesso em $arquivo_saida."
+    ;;
+ "descompactar")
+    read -p "Nome do arquivo a descompactar (.tar.gz): " arquivo
+    read -p "Diretório de destino para a descompactação: " diretorio_destino
+    if [ ! -e "$arquivo" ]; then
+      echo "Arquivo não encontrado: $arquivo"
+      exit 1
+    fi
+    tar -xzf "$arquivo" -C "$diretorio_destino"
+    echo "Arquivo descompactado com sucesso em $diretorio_destino."
+    ;;
+  *)
+    echo "Operação inválida."
+    echo "Uso: $0 (compactar|descompactar)"
+    exit 1
+    ;;
 esac
